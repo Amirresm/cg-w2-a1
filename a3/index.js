f017bce22348a03996fc9ab495ec9bb9cf5c5b3d
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2024-03-17 01:08:55 -0700

    fix alot

--- a3/index.js
+++ a3/index.js
@@ -1,6 +1,38 @@
+function randomPointOnSphere(ox, oy, oz, radius) {
+	const randomLat = Math.floor(Math.random() * 180);
+	const randomLong = Math.floor(Math.random() * 360);
+
+	const theta = radians(randomLat);
+	const sinTheta = Math.sin(theta);
+	const cosTheta = Math.cos(theta);
+	const phi = radians(randomLong);
+	const sinPhi = Math.sin(phi);
+	const cosPhi = Math.cos(phi);
+	const x = ox + radius * cosPhi * sinTheta;
+	const y = oy + radius * cosTheta;
+	const z = oz + radius * sinPhi * sinTheta;
+	return { x, y, z, lat: randomLat, long: randomLong }
+}
+
+function pointOnSphere(ox, oy, oz, radius, latNumber, longNumber) {
+	const randomLat = 90 + latNumber * 180;
+	const randomLong = 270 + longNumber * 360;
+
+	const theta = radians(randomLat);
+	const sinTheta = Math.sin(theta);
+	const cosTheta = Math.cos(theta);
+	const phi = radians(randomLong);
+	const sinPhi = Math.sin(phi);
+	const cosPhi = Math.cos(phi);
+	const x = ox + radius * cosPhi * sinTheta;
+	const y = oy + radius * cosTheta;
+	const z = oz + radius * sinPhi * sinTheta;
+	return { x, y, z, lat: randomLat, long: randomLong }
+}
+
 function main() {
-	const resolution = 10;
-	const renderPipeline = new RenderPipeline("webgl", resolution, resolution * 2, 2);
+	const resolution = 100;
+	const renderPipeline = new RenderPipeline("webgl", resolution, resolution * 2, 10);
 
 	const canvas = document.getElementById("webgl");
 
@@ -17,34 +49,49 @@ function main() {
 	window.onmouseup = function() {
 		dragging = false;
 	}
+	let dx = 0;
+	let dy = 0;
+	let zoom = 6
 	window.onmousemove = function(ev) {
 		if (dragging) {
 			let x = ev.clientX;
 			let y = ev.clientY;
-			let factor = 100 / canvas.height;
-			let dx = factor * (x - dragStartX);
-			let dy = factor * (y - dragStartY);
-			console.log(dx, dy);
-			renderPipeline.viewMatrix.rotate(dy, 1, 0, 0);
-			renderPipeline.viewMatrix.rotate(dx, 0, 1, 0);
+			let factor = 0.5 / canvas.height;
+			dx += factor * (x - dragStartX);
+			dy += factor * (y - dragStartY);
+			if (dy > 0.495) dy = 0.495;
+			if (dy < -0.495) dy = -0.495;
+			const { x: ex, y: ey, z: ez } = pointOnSphere(0, 0, 0, zoom, -dy, dx);
+			renderPipeline.viewMatrix.setLookAt(ex, ey, ez, 0, 0, 0, 0, 1, 0)
 			dragStartX = x;
 			dragStartY = y;
 		}
 	}
+	window.addEventListener("wheel", event => {
+		const delta = Math.sign(event.deltaY);
+		zoom += delta * 0.5;
+		const { x: ex, y: ey, z: ez } = pointOnSphere(0, 0, 0, zoom, -dy, dx);
+		renderPipeline.viewMatrix.setLookAt(ex, ey, ez, 0, 0, 0, 0, 1, 0)
+	});
 
 
 	renderPipeline.setColor({
 		sphere: [0.3, 0.3, 0.3, 1.0],
-		dot: [1.0, 1.0, 0.0, 1.0],
+		dot: [1.0, 0.0, 0.0, 1.0],
 		bacteria: [0.0, 1.0, 0.0, 1.0],
 	})
 
-	renderPipeline.setDotPosition(resolution / 5, resolution / 5);
+	renderPipeline.setDotPosition(4, 4);
 	renderPipeline.setBacteriaPosition(2, 100);
 
-	renderPipeline.addSphere(0, 0, 0, 2);
-	renderPipeline.addSphere(2, 2, 0, 0.5);
-	renderPipeline.viewMatrix.setTranslate(0, 0, -6);
+	renderPipeline.addSphere(0, 0, 0, 3.0);
+
+
+	const bacteriaPos = randomPointOnSphere(0, 0, 0, 2.95);
+	renderPipeline.addSphere(bacteriaPos.x, bacteriaPos.y, bacteriaPos.z, 0.1, "bact");
+	// renderPipeline.viewMatrix.setTranslate(0, 0, -6);
+	const { x: ex, y: ey, z: ez } = pointOnSphere(0, 0, 0, 6, 0, 0);
+	renderPipeline.viewMatrix.setLookAt(ex, ey, ez, 0, 0, 0, 0, 1, 0);
 
 	let theta = 0.0;
 	function render() {
--- a3/renderPipeline.js
+++ a3/renderPipeline.js
@@ -24,7 +24,7 @@ class RenderPipeline {
 		float pointScaleRate = 1.00;
 		mat4 pointScale = mat4(pointScaleRate, 0.0, 0.0, 0.0,   0.0, pointScaleRate, 0.0, 0.0,   0.0, 0.0, pointScaleRate, 0.0,   0.0, 0.0, 0.0, 1.0);
 		if (type == 0.0) {
-			gl_PointSize = 0.0;
+			gl_PointSize = 5.0;
 			vColor = uSphereColor;
 		} else if (type == 1.0) {
 			gl_PointSize = 4.0;
@@ -46,7 +46,7 @@ class RenderPipeline {
 	gl_FragColor = vColor;
 	}
   `;
-	constructor(canvasId, latStep, langStep, sphereCount) {
+	constructor(canvasId, latStep, langStep, maxSphereCount) {
 		this.canvas = document.getElementById(canvasId);
 		this.gl = getWebGLContext(this.canvas);
 		if (!this.gl) {
@@ -65,13 +65,16 @@ class RenderPipeline {
 
 		this.latStep = latStep;
 		this.langStep = langStep;
-		this.sphereCount = sphereCount;
+		this.maxSphereCount = maxSphereCount;
+		this.currentSphereCount = 0;
 
 		this.vertexSize = 3 + 1;
-		this.verticesCount = latStep * langStep;
+		this.verticesCount = latStep * langStep + 2;
+		this.indicesCount = (latStep) * langStep * 6 + 2 * langStep * 3;
 		this.dataBufferOffset = null;
-		this.resizeDataBuffer(sphereCount * this.verticesCount * this.vertexSize);
+		this.resizeDataBuffer(maxSphereCount * this.verticesCount * this.vertexSize);
 		this.indices = [];
+		this.lineIndices = [];
 	}
 
 	resizeDataBuffer(size) {
@@ -93,65 +96,101 @@ class RenderPipeline {
 		this.dotPos = { phi, theta };
 	}
 
-	addSphere(ox, oy, oz, radius, type = "field") {
-		const latDegreeStep = 180 / (this.latStep - 1);
-		const langDegreeStep = 360 / (this.langStep);
+	createSphere(ox, oy, oz, radius, latCount, longCount) {
+		const latDegreeStep = 180 / (latCount + 1);
+		const longDegreeStep = 360 / (longCount);
 
-		let bufferOffset = this.dataBufferOffset;
-		for (let latNumber = 0; latNumber < this.latStep; latNumber++) {
-			const theta = latNumber * latDegreeStep + 0;
-			const sinTheta = Math.sin(radians(theta));
-			const cosTheta = Math.cos(radians(theta));
-			for (let langNumber = 0; langNumber <= this.langStep; langNumber++) {
-				const phi = langNumber * langDegreeStep;
-				const sinPhi = Math.sin(radians(phi));
-				const cosPhi = Math.cos(radians(phi));
-				const x = cosPhi * sinTheta;
-				const y = sinPhi * sinTheta;
-				const z = cosTheta;
+		const points = [];
+		points.push({ x: ox, y: oy + radius, z: oz, lat: 0, long: 0 });
+		for (let latIter = 1; latIter < latCount + 1; latIter++) {
+			const theta = radians(latIter * latDegreeStep);
+			const sinTheta = Math.sin(theta);
+			const cosTheta = Math.cos(theta);
+			for (let longIter = 0; longIter < longCount; longIter++) {
+				const phi = radians(longIter * longDegreeStep);
+				const sinPhi = Math.sin(phi);
+				const cosPhi = Math.cos(phi);
+				const x = ox + radius * cosPhi * sinTheta;
+				const y = oy + radius * cosTheta;
+				const z = oz + radius * sinPhi * sinTheta;
+				points.push({ x, y, z, lat: latIter, long: longIter })
+			}
+		}
+		points.push({ x: ox, y: oy - radius, z: oz, lat: latCount + 1, long: 0 });
+		return points;
+	}
 
-				let bIndex = bufferOffset + (latNumber * this.langStep + langNumber) * this.vertexSize;
-				this.dataBuffer[bIndex++] = ox + x * radius;
-				this.dataBuffer[bIndex++] = oy + y * radius;
-				this.dataBuffer[bIndex++] = oz + z * radius;
-				if (type === "field") {
-					if (latNumber % this.dotPos.phi === 0 && langNumber % this.dotPos.theta === 0) {
-						this.dataBuffer[bIndex++] = 1.0;
-					} else if (latNumber === this.bacteriaPos.phi && langNumber === this.bacteriaPos.theta) {
-						this.dataBuffer[bIndex++] = 2.0;
-					} else {
-						this.dataBuffer[bIndex++] = 0.0;
-					}
-				} else if (type === "bact") {
-					this.dataBuffer[bIndex++] = 2.0;
+	addSphere = (ox, oy, oz, radius, type = "field") => {
+		let bufferOffset = this.currentSphereCount * this.verticesCount * this.vertexSize;
+
+
+		const spherePoints = this.createSphere(ox, oy, oz, radius, this.latStep, this.langStep);
+		spherePoints.forEach((point, i) => {
+			const { lat, long } = point;
+			let bIndex = bufferOffset + i * this.vertexSize;
+			this.dataBuffer[bIndex++] = point.x;
+			this.dataBuffer[bIndex++] = point.y;
+			this.dataBuffer[bIndex++] = point.z;
+
+			if (type === "field") {
+				if (lat % this.dotPos.phi === 0 && long % this.dotPos.theta === 0) {
+					this.dataBuffer[bIndex++] = 1.0;
+				} else if (lat === this.bacteriaPos.phi && long === this.bacteriaPos.theta) {
+					this.dataBuffer[bIndex++] = 0.0;
+				} else {
+					this.dataBuffer[bIndex++] = 0.0;
 				}
+			} else if (type === "bact") {
+				this.dataBuffer[bIndex++] = 2.0;
+			}
+		});
+
+		const bufferVertexOffset = (this.currentSphereCount * this.verticesCount);
+		for (let point of spherePoints) {
+			const latIndex = point.lat - 1;
+			if (point.lat === 0) {
+				for (let i = 0; i < this.langStep; i++) {
+					const start = 0;
+					const p1 = 1 + i;
+					const p2 = 1 + ((i + 1) % this.langStep);
+					this.indices.push(bufferVertexOffset + start);
+					this.indices.push(bufferVertexOffset + p1);
+					this.indices.push(bufferVertexOffset + p2);
+				}
+			} else if (point.lat === this.latStep + 1) {
+				for (let i = 0; i < this.langStep; i++) {
+					const end = this.latStep * this.langStep + 1;
+					const p1 = 1 + ((this.latStep - 1) * this.langStep) + i;
+					const p2 = 1 + ((this.latStep - 1) * this.langStep) + ((i + 1) % this.langStep);
+					this.indices.push(bufferVertexOffset + end);
+					this.indices.push(bufferVertexOffset + p1);
+					this.indices.push(bufferVertexOffset + p2);
+				}
+			} else {
+				const row11 = 1 + (latIndex * this.langStep) + point.long;
+				const row12 = 1 + (latIndex * this.langStep) + (point.long + 1) % this.langStep;
+				const row21 = 1 + ((latIndex + 1) % this.latStep * this.langStep) + point.long;
+				const row22 = 1 + ((latIndex + 1) % this.latStep * this.langStep) + (point.long + 1) % this.langStep;
+
+				this.indices.push(bufferVertexOffset + row11);
+				this.indices.push(bufferVertexOffset + row12);
+				this.indices.push(bufferVertexOffset + row21);
+
+				this.indices.push(bufferVertexOffset + row12);
+				this.indices.push(bufferVertexOffset + row22);
+				this.indices.push(bufferVertexOffset + row21);
 			}
 		}
 
-		const latRectCount = this.latStep;
-		const langRectCount = this.langStep;
-		bufferOffset = 1 * this.dataBufferOffset;
-		for (let j = 0; j < latRectCount - 0; j++) {
-			for (let i = 0; i < langRectCount; i++) {
-				const q1 = i + j * langRectCount;
-				const q2 = (i + 1) % langRectCount + j * langRectCount;
-				const q3 = i + (j + 1) * langRectCount;
-				const q4 = (i + 1) % langRectCount + (j + 1) * langRectCount;
-
-				this.indices.push(bufferOffset + q1);
-				this.indices.push(bufferOffset + q3);
-				this.indices.push(bufferOffset + q4);
-				this.indices.push(bufferOffset + q1);
-				this.indices.push(bufferOffset + q2);
-				this.indices.push(bufferOffset + q4);
-			}
-		}
 		this.dataBufferOffset += this.verticesCount * this.vertexSize;
 		this.indexBuffer = new Uint16Array(this.indices);
-		if (this.indexBufferCount === undefined) this.indexBufferCount = this.indexBuffer.length;
-		console.log(this.dataBuffer.length)
-		console.log(this.dataBufferOffset)
-		console.log(this.indexBuffer.length)
+		// console.log(this.dataBuffer.length)
+		// console.log(this.dataBufferOffset)
+		// console.log(bufferVertexOffset)
+		// console.log(this.indexBuffer.length)
+		// console.log(this.indicesCount)
+		// console.log(this.indices.slice((this.currentSphereCount) * this.indicesCount, (this.currentSphereCount + 1) * this.indicesCount))
+		this.currentSphereCount += 1;
 	}
 
 	render() {
@@ -199,8 +238,9 @@ class RenderPipeline {
 
 		gl.enable(gl.DEPTH_TEST);
 		gl.clear(gl.COLOR_BUFFER_BIT);
-		for (let i = 0; i < this.sphereCount; i++) {
-			gl.drawElements(gl.TRIANGLES, this.indexBufferCount, gl.UNSIGNED_SHORT, this.indexBufferCount);
+		gl.drawElements(gl.TRIANGLES, this.indicesCount * this.currentSphereCount, gl.UNSIGNED_SHORT, 0);
+		for (let i = 0; i < this.currentSphereCount; i++) {
+			// gl.drawElements(gl.TRIANGLES, this.indicesCount, gl.UNSIGNED_SHORT, i * this.indicesCount * Uint16Array.BYTES_PER_ELEMENT);
 			// gl.drawArrays(
 			// 	gl.POINTS,
 			// 	i * this.verticesCount,
